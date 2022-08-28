8ec47637f229f9fe0e482605e6dd8d7b26f7b698
jihch <haoqibaobaoji@126.com>
2022-08-29 04:39:27 +0800

    1

---
+++ kafka-spring-boot-demo/src/main/java/io/github/jihch/consumer/MyConsumer.java
@@ -0,0 +1,53 @@
+package io.github.jihch.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyConsumer {
+
+    /*
+    @KafkaListener(topics = "my-replicated-topic", groupId = "MyGroup1")
+    public void listenGroup(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        String value = record.value();
+        System.out.println(value);
+        System.out.println(record);
+
+        //手动提交 offset
+        ack.acknowledge();
+    }
+     */
+
+    @KafkaListener(groupId = "testGroup", topicPartitions = {
+            @TopicPartition(topic = "topic1", partitions = {"0", "1"}),
+            @TopicPartition(topic = "topic2", partitions = "0",
+            partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "100"))
+    }, concurrency = "3") //concurrency 就是同组下的消费者个数，就是并发消费数，建议小于等于分区数
+    public void listenGroupPro(ConsumerRecord<String, String> record, Acknowledgment ack) {
+
+        String value = record.value();
+        System.out.println(value);
+        System.out.println(record);
+
+        //手动提交 offset
+        ack.acknowledge();
+    }
+
+    /*
+    @KafkaListener(topics = "my-replicated-topic", groupId = "MyGroup1")
+    public void listensGroup(ConsumerRecord<String, String> records, Acknowledgment ack) {
+        String value = record.value();
+        System.out.println(value);
+        System.out.println(record);
+
+        //手动提交 offset
+        ack.acknowledge();
+    }
+
+     */
+
+}
---
+++ kafka-spring-boot-demo/src/test/java/io/github/jihch/KafkaSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jihch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
